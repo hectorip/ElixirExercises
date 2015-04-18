6aa3cb14841e60b20b60abce37e432509ddeae50
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-04-18 14:03:51 -0500

    capturing values

--- PatternMatching.ex
+++ PatternMatching.ex
@@ -10,3 +10,23 @@ IO.puts "day #{day}"
 IO.puts "Hour #{hour}"
 IO.puts "Minute #{minute}"
 IO.puts "Second #{second}"
+
+
+# Reading a file
+
+{:ok, contents} = File.read("my_files.txt")
+
+IO.puts contents
+
+# Patterns
+
+{_, time} = :calendar.local_time
+
+IO.puts elem(time, 2)
+
+IO.puts "Capturing values"
+Enum.each(Tuple.to_list(time), &IO.puts/1)
+IO.puts "Finishing."
+{_,{hour,_,_}} = :calendar.local_time
+
+IO.puts hour
