a0328af5349e5bc47ce0a948a00d65972fb8bdca
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-04-18 11:32:14 -0500

    Documentation

--- AttributesInModules.ex
+++ AttributesInModules.ex
@@ -1,5 +1,10 @@
 defmodule Circle do
+  @moduledoc "Implements basic circle fucntions"
   @pi 3.141592
+
+  @doc "Computes the area of the circe"
   def area(r), do: r*r*@pi
+
+  @doc "Computes circumference of the circle"
   def circunference(r), do: 2*r*@pi
 end
