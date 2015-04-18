3967aba4b8c75cd38b99f30def199e38a5f807d7
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-04-18 12:59:40 -0500

    Modifying and Adding values in Maps

--- Maps.ex
+++ Maps.ex
@@ -12,3 +12,14 @@ IO.puts bob[:age]
 
 IO.puts erik[:name]
 IO.puts erik[:non_existent_field]
+
+
+# Adding elements to a Map
+
+bob = %{bob | :age => 50000}
+
+IO.puts bob.age
+
+bob = Map.put(bob, :salary, 70000)
+
+IO.puts bob.salary
