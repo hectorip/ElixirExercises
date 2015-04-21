ed59677139d9a97ad841fe4e6fdb32e1c521ef18
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-04-20 23:18:28 -0500

    Pin operator explained

--- PinOperator.ex
+++ PinOperator.ex
@@ -5,7 +5,9 @@ IO.puts name
 IO.puts age
 {name, age} = {"Bobo", 23}
 x = 2
-{x, ^x} = {2,1}
-IO.puts "X is #{x}"
+{x, ^x} = {1,2}
+# Now this works and if the pattern matching is right, it will assign
+# the value to the variable after finishing the check of all
+IO.puts "X is #{x}" # X must be 1
 IO.puts name
 IO.puts age
