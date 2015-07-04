7aecd65d7805d58c294d6ba5d4a99b0f40b131e6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-04 18:49:04 -0500

    Destructuration on tuples

--- Tuples.ex
+++ Tuples.ex
@@ -5,8 +5,18 @@ age = elem(person, 1)
 #IO.puts person
 IO.puts age
 
-person = put_elem(person, 1, 43)
+person_2 = put_elem(person, 1, 43)
 
 # IO.puts person
-age = elem(person, 1)
+age = elem(person_2, 1)
 IO.puts age
+
+
+# Destructuration
+
+{a, b, c, d} = {"a", "b", "c", "d"}
+
+IO.puts a
+IO.puts b
+IO.puts c
+IO.puts d
