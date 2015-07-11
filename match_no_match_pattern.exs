04e6dfdc7ac41356488fc4653142699d36ae893d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-10 23:57:52 -0500

    match and not match

---
+++ match_no_match_pattern.exs
@@ -0,0 +1,22 @@
+
+# will match
+a = [1,2,3]
+
+#will match
+a = 4
+
+# will match
+4=a
+
+# will not match
+[a,b]=[1,2,3] 
+
+# will not match
+
+a=[[1,2,3]]
+
+# will match
+[a]=[[1,2,3]]
+
+#will not match
+[[a]]=[[1,2,3]]
