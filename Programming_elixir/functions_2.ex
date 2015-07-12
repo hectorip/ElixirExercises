c567327835b48a1c9a996c407a1d6ba4d3f23bc2
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-11 20:27:57 -0500

    fizz buzz complete

--- Programming_elixir/functions_2.ex
+++ Programming_elixir/functions_2.ex
@@ -1,13 +1,28 @@
 # fizz Buzz
 
-fizz_buzz = fn
+get_word = fn
   0, 0, _ -> "FizzBuzz"
   0, _, _ -> "Fizz"
   _, 0, _ -> "Buzz"
   _, _, a -> a
 end
+IO.puts("Testing first function")
+IO.puts get_word.(0,0,2)
+IO.puts get_word.(0,1,2)
+IO.puts get_word.(1,0,2)
+IO.puts get_word.(1,1,100)
 
-IO.puts fizz_buzz.(0,0,2)
-IO.puts fizz_buzz.(0,1,2)
-IO.puts fizz_buzz.(1,0,2)
-IO.puts fizz_buzz.(1,1,100)
+IO.puts("\nTesting FizzBuzz")
+
+fizz_buzz = fn number ->
+  get_word.(rem(number, 3), rem(number, 5), number)
+end
+
+IO.puts fizz_buzz.(10)
+IO.puts fizz_buzz.(11)
+IO.puts fizz_buzz.(12)
+IO.puts fizz_buzz.(13)
+IO.puts fizz_buzz.(14)
+IO.puts fizz_buzz.(15)
+IO.puts fizz_buzz.(16)
+IO.puts fizz_buzz.(17)
