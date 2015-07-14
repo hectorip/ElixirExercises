d2aefd4e5cd36ba6bb94c2e690246579178a9755
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-14 07:15:59 -0500

    & operator

---
+++ Programming_elixir/ampersand_operator.exs
@@ -0,0 +1,6 @@
+# adding 2 to every element in a list
+Enum.map [1, 2, 3, 4], fn x -> x + 2 end
+Enum.map [1, 2, 3, 4], &(&1 + 2)
+
+Enum.each [1, 2, 3, 4], fn x -> IO.inspect x end
+Enum.each [1, 2, 3, 4], &IO.inspect/1
