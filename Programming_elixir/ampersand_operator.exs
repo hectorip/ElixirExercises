2a72c4ec169286eef7917acc8adc46b1617ac1be
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-14 07:49:16 -0500

    named modules

--- Programming_elixir/ampersand_operator.exs
+++ Programming_elixir/ampersand_operator.exs
@@ -1,7 +1,12 @@
 # adding 2 to every element in a list
 Enum.map [1, 2, 3, 4], fn x -> x + 2 end
-Enum.map [1, 2, 3, 4], &(&1 + 2)
+new = Enum.map [1, 2, 3, 4], &(&1 + 2)
+
+IO.puts("Second example")
 
 Enum.each [1, 2, 3, 4], fn x -> IO.inspect x end
 Enum.each [1, 2, 3, 4], &IO.inspect/1
 Enum.each [1, 2, 3, 4], &(IO.inspect &1)
+
+IO.puts("Inspect element created by the first function")
+Enum.each new, &(IO.inspect &1)
