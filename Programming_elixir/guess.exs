81f48e855347a3e8e13ad711b8837e265a391e5d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-14 23:56:03 -0500

    Guessing

---
+++ Programming_elixir/guess.exs
@@ -0,0 +1,25 @@
+defmodule Guess do
+  def guess(actual, range) do
+    _..last = range
+    my_guess = div(last, 2)
+    compare(actual, my_guess, range)
+  end
+
+  def compare(actual, guessed, range) when actual == guessed do
+    IO.puts guessed
+  end
+  def compare(actual, guessed, range) when actual > guessed do
+    IO.puts "It's #{guessed}"
+    _..last = range
+    new_range = guessed..last
+    guess(actual,new_range)
+  end
+
+  def compare(actual, guessed, range) when actual < guessed do
+    IO.puts "It's #{guessed}"
+    first.._ = range
+    new_range = first..guessed
+    guess(actual,new_range)
+  end
+end
+
