1c1bd0d6f217b6f8742c9c0e78de871304460994
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-16 00:26:29 -0500

    Modules and atoms

--- Programming_elixir/Modules.exs
+++ Programming_elixir/Modules.exs
@@ -2,6 +2,7 @@
 # Attributes have the same value in code until another attribute sets 
 # the value and all the functions after that has the new value
 defmodule ExampleModule do
+
   @this_is_an_attribute "attr1"
   def func_1, do: IO.puts @this_is_an_attribute
   
---
+++ Programming_elixir/Modules_atoms.exs
@@ -0,0 +1,5 @@
+IO.puts(is_atom IO)
+IO.puts(to_string IO)
+IO.puts(:"Elixir.IO" == IO)
+
+:"Elixir.IO".puts "Modules are atoms"
