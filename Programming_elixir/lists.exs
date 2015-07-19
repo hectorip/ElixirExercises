8638d75074e6693da56f167c12aa0cc30304ca1b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-18 23:32:37 -0500

    mapsum

--- Programming_elixir/lists.exs
+++ Programming_elixir/lists.exs
@@ -2,6 +2,8 @@ defmodule MyList do
     def sum([]), do: 0
     def sum([head | tail]), do: head + sum(tail)
 
+    def mapsum([], _op), do: 0
+    def mapsum([head | tail], op), do: op.(head) + mapsum(tail, op)
 
 end
 
@@ -10,4 +12,10 @@ test_list = [10, 11, 12, 13, 14, 15]
 IO.inspect(test_list)
 IO.puts(MyList.sum(test_list))
 
+IO.puts("Testing mapsum")
+test_list_2 = [1, 2, 3, 4]
+IO.inspect(test_list_2)
+op = fn x -> x * x end
+
+IO.puts(MyList.mapsum(test_list_2, op))
 
