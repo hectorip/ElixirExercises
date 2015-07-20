13156b2309047698898afc9f838a38ad40e7b970
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-19 23:54:11 -0500

    PLaying with Dict API

--- Programming_elixir/dict_api.exs
+++ Programming_elixir/dict_api.exs
@@ -9,3 +9,14 @@ IO.puts SumDict.values(hash_test)
 
 map_test = %{five: 4, six: 6, ten: 10}
 IO.puts SumDict.values(map_test)
+
+IO.puts "Playing with Dict API"
+# another way to create Maps and hashes
+
+collection = [name: "Héctor", age: 26, language: [:PHP, :Python, :Ruby, :Elixir]]
+
+col_map = Enum.into collection, Map.new
+IO.inspect col_map
+
+col_hd = Enum.into collection, HashDict.new
+IO.inspect col_hd
