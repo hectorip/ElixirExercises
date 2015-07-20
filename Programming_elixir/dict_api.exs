f531b2beca88018a5d1ff827b338f6d42cb72393
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-19 23:49:21 -0500

    Map sum test

--- Programming_elixir/dict_api.exs
+++ Programming_elixir/dict_api.exs
@@ -6,3 +6,6 @@ end
 
 hash_test = [one: 1, two: 2, three: 3, four: 4] |> Enum.into HashDict.new
 IO.puts SumDict.values(hash_test)
+
+map_test = %{five: 4, six: 6, ten: 10}
+IO.puts SumDict.values(map_test)
