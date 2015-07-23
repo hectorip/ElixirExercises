bb6d0c75dfe6b21cca03faa85174c845e3981369
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-22 23:58:34 -0500

    updating dictionaries

--- Programming_elixir/nested_access.exs
+++ Programming_elixir/nested_access.exs
@@ -1,6 +1,16 @@
+# The file structs must be compiled and loades in order to
+# make this file works
+
 import User
 import Group
 
 user_1 = %User{name: "Hector"}
 group_1 = %Group{director: user_1, size: 1}
 
+
+IO.puts group_1.director.name
+
+# updating a struct 
+user_1 = %User{ user_1 | name: "Iván"}
+IO.puts user_1.name
+IO.puts group_1.director.name
