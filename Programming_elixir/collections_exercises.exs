4c795f701d76549023a3da7bce93351b4d1577d6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-25 00:03:01 -0500

    Enums.all?

--- Programming_elixir/collections_exercises.exs
+++ Programming_elixir/collections_exercises.exs
@@ -2,9 +2,9 @@
 defmodule Enums do
   def all?([],_f), do: false
   def all?(list, f) do
-    Enums._all(list, f, true)
+    _all(list, f, true)
   end
-  defp([], _, acc), do: acc
+  defp _all([], _f, acc), do: acc
   defp _all([head|tail], f, acc) do
     _all(tail, f, f.(head) and acc)
   end
