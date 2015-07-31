1652b9998a1fe328345ba5197aa67302dc272647
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-07-30 23:25:07 -0500

    Anagram

--- Programming_elixir/StringsAndBinaries.exs
+++ Programming_elixir/StringsAndBinaries.exs
@@ -6,4 +6,12 @@ defmodule SB do
   defp all_p([h|t], acc) do
     acc and all_p(t, h >= 32 and h <= 126)
   end
+  def anagram?([],[]), do true
+  def anagram?(word1, word2) when word1 == [] or word2 == [], do: false
+  end
+  def anagram?(word1, word2) do
+    [h, word1] = word1
+    word2 = word2 -- [h]
+    anagram?(word1, word2)
+  end
 end
