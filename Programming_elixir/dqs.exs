4cebd121ff8ad0e05f3328a07608434b0e842fbc
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-08-01 00:14:40 -0500

    Not allowed expression in Guard

--- Programming_elixir/dqs.exs
+++ Programming_elixir/dqs.exs
@@ -1,9 +1,14 @@
 defmodule Dqs do
   def center(list) do
-    
+    lg = get_longest(list, 0)
+    for w <- lg, do: IO.puts(String.rjust(w,lg))
   end
   def get_longest(_l, lg), do: lg
-  def get_longest(list, lg) do
-    
+  def get_longest([h|t], lg) when String.length h >= lg do
+    get_longest(t, String.length h)
+  end
+  def get_longest([h|t], lg) when String.length h >= lg do
+    get_longest(t, lg)
+
   end
 end
