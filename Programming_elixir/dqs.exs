737bd33012236b86e60b6aa2c12e4bef51eaef9a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-08-01 00:29:13 -0500

    I missed a detail

--- Programming_elixir/dqs.exs
+++ Programming_elixir/dqs.exs
@@ -1,14 +1,18 @@
 defmodule Dqs do
   def center(list) do
     lg = get_longest(list, 0)
-    for w <- lg, do: IO.puts(String.rjust(w,lg))
+    print_l(list, lg)
   end
-  def get_longest(_l, lg), do: lg
-  def get_longest([h|t], lg) when String.length h >= lg do
-    get_longest(t, String.length h)
+  def print_l([h|t], lg) when h != [] do
+    IO.puts String.rjust(h, lg)
+    print_l(t, lg)
   end
-  def get_longest([h|t], lg) when String.length h >= lg do
-    get_longest(t, lg)
-
+  def get_longest([], lg), do: lg
+  def get_longest([h|t], lg) do
+    if String.length(h) >= lg do
+      get_longest(t, String.length h)
+    else
+        get_longest(t, lg)
+    end
   end
 end
