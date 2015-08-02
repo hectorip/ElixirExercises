24d6808dbc273c0cfe7cf6539ac053c1195dc5fd
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-08-01 23:44:09 -0500

    Cleaning up - using a closure

--- Programming_elixir/dqs.exs
+++ Programming_elixir/dqs.exs
@@ -1,12 +1,11 @@
 defmodule Dqs do
   def center(list) do
     lg = get_longest(list, 0)
-    print_l(list, lg)
-  end
-  def print_l([h|t], lg) when h != [] do
-    l = String.length(h) + div(lg-String.length(h), 2)
-    IO.puts String.rjust(h, l)
-    print_l(t, lg)
+    print_l = fn h -> 
+       l = String.length(h) + div(lg-String.length(h), 2)
+      IO.puts String.rjust(h, l)
+    end
+    Enum.each(list, print_l)
   end
   def get_longest([], lg), do: lg
   def get_longest([h|t], lg) do
