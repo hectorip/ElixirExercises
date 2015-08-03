23cce352d961b1b474daa7acdfa8fda6e0e87488
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-08-02 23:59:16 -0500

    FizzBuzz

--- Programming_elixir/control_flow.exs
+++ Programming_elixir/control_flow.exs
@@ -2,8 +2,10 @@ defmodule ControlFlow do
   def fizz_buzz(up_to) do
     for x <- 1..up_to do
       case x do
-        number = x when 0 == rem(number,15) -> "FizzBuzz"
-        number = x -> number
+        x when 0 == rem(x,15) -> "FizzBuzz"
+        x when 0 == rem(x, 3) -> "Fizz"
+        x when 0 == rem(x, 5) -> "Buzz"
+        x -> x
       end
     end
   end
