03060e6c79ce92ca8f1e4adbaf7937c954a40e10
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-09-27 23:51:44 -0500

    Linking processes

---
+++ process/link_1.exs
@@ -0,0 +1,20 @@
+defmodule Link1 do
+  import :timer, only: [ sleep: 1 ]
+
+  def sad_function do
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn(Link1, :sad_function, [])
+
+    receive do
+      msg ->
+        IO.puts "Message Received: #{inspect msg}"
+    after 1000 ->
+      IO.puts "Nothing happened as far as I am concerned"
+    end
+  end
+end
---
+++ process/link_2.exs
@@ -0,0 +1,18 @@
+defmodule Link2 do
+  import :timer, only: [ sleep: 1 ]
+  def sad_function do
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    spawn_link(Link2, :sad_function, [])
+
+    receive do
+      msg ->
+        IO.puts("Message Reaceived: #{inspect msg}")
+    after 1000 ->
+      IO.puts("Nothing happened as far as I am concerned")
+    end
+  end
+end
