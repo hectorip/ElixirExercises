38f0d05c3b6fbf815b41f927d32de695637fe330
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-07 23:59:03 -0500

    link process exercise

--- process/messages_3.exs
+++ process/messages_3.exs
@@ -1,8 +1,17 @@
 defmodule MessageQueue do
   def send_message(pid) do
     send pid, ":)"
+    exit(:boom)
+  end
+
+  def run() do
+    pid = spawn_link(MessageQueue, :send_message, [])
+    import :timer, only: [ sleep: 1 ]
+    sleep 500
+    receive do
+      msg -> 
+       IO.puts msg
+    end
   end
 end
 
-
-pid = spawn_link(MessageQueue, :send_message, [])
