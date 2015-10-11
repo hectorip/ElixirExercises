777b36d0a6664748509812c77ee808abfb3aa83d
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-11 02:09:25 -0500

    Linked processes raisin exception

---
+++ process/messages_4.exs
@@ -0,0 +1,23 @@
+defmodule MessageQueue do
+  def send_message(pid) do
+    send pid, ":)"
+    raise "oooops"
+  end
+
+  def receiveMessages() do
+    receive do
+      msg ->
+        IO.inspect msg
+        receiveMessages()
+    end
+  end
+
+  def run() do
+    Process.flag(:trap_exit, true)
+    spawn_link(MessageQueue, :send_message, [self])
+    import :timer, only: [ sleep: 1 ]
+    sleep 500
+    receiveMessages()
+  end
+end
+
