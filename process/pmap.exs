9d30ab661a00b3b464d1c96b90a351622635885b
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-15 00:00:33 -0500

    Excersice 6

--- process/pmap.exs
+++ process/pmap.exs
@@ -1,6 +1,6 @@
 defmodule Parallel do
   def pmap(collection, fun) do
-    me = self
+    me = self # this is because self word would take a nother meaning inside the spawned function
     collection
     |> Enum.map(fn (elem) ->
         spawn_link fn -> (send me, {self, fun.(elem) }) end
