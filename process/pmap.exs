7f6a3946552b7b1e2b5e3c60af4e0825f5541706
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-16 23:59:22 -0500

    bad pmap

--- process/pmap.exs
+++ process/pmap.exs
@@ -10,4 +10,15 @@ defmodule Parallel do
     end)
     # now I understand
   end
+
+  def bad_pmap(collection, fun) do
+    me = self
+    collection
+    |> Enum.map(fn (elem) ->
+        spawn_link fn -> (send me, { self, fun.elem() }) end
+    end)
+    |> Enum.map(fn (pid) ->
+      receive do { _pid, result } -> result end
+    end)
+  end
 end
