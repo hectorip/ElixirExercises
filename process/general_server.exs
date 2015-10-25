cf8a8afc4c28ffeea7576eb38205bf00a4a4b4ef
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-24 23:59:16 -0500

    Redsigning general server and scheduler

--- process/general_server.exs
+++ process/general_server.exs
@@ -1,6 +1,6 @@
-defmodule GenServer do
+defmodule GeneralServer do
 
-  def worker(f, scheduler,args) do
+  def worker(scheduler, f,args) do
     send scheduler, {:ready, self}
     receive do
       {:call, f, args, client} ->
@@ -11,4 +11,9 @@ defmodule GenServer do
   end
 end
 
-
+defmodule GeneralScheduler do
+  def run(num_processes, module, f, args) do
+    (1..num_processes)
+    |> Enum.map(fn (_) -> spawn(module, :worker, [self, f, args]))
+  end
+end
