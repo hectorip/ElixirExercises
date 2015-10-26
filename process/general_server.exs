16148e3c939ce17c362a2fe5de69f975c42f58e3
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-25 23:43:05 -0600

    First version of genral workers

--- process/general_server.exs
+++ process/general_server.exs
@@ -1,19 +1,48 @@
 defmodule GeneralServer do
 
-  def worker(scheduler, f,args) do
+  def worker(scheduler) do
     send scheduler, {:ready, self}
     receive do
-      {:call, f, args, client} ->
-        send client, {:answer, args, f.(args)}
-        worker(f, scheduler, args)
+      {:call, file, client} ->
+        send client, {:answer, file, search_cat(file)}
+        worker(scheduler)
       {:shutdown} ->
         exit(:normal)
+    end
+  end
+
+  defp search_cat(file) do
+    # Here we will search for cat
+    content = File.read! file
+    1
   end
 end
 
 defmodule GeneralScheduler do
-  def run(num_processes, module, f, args) do
-    (1..num_processes)
-    |> Enum.map(fn (_) -> spawn(module, :worker, [self, f, args]))
+  def run(arg) do
+    files = File.ls! arg
+    IO.inspect files
+    (1..Enum.count files)
+    |> Enum.map(fn (_) -> spawn(GeneralServer, :worker, [self]) end)
+    |> schedule_processes(files, 0)
+  end
+
+  defp schedule_processes(processes, to_process,res) do
+    receive do
+      {:ready, pid } when length(to_process) > 0 ->
+        [ next | tail ] = to_process
+        send pid, {:call, next, self}
+        schedule_processes(processes, tail, res)
+      {:ready, pid } ->
+        send pid, {:shutdown}
+        if length(processes) > 1 do
+          schedule_processes(List.delete(processes, pid), to_process, res)
+        else
+          res
+        end
+      {:answer, file, result, _pid} ->
+        res = res + result
+        schedule_processes(processes, to_process, res)
+    end
   end
 end
