a44ae241e8a8ed6bfedf12db13fa4b6b9a7a6eca
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-16 00:37:25 -0600

    First ring client attempt

--- nodes/ticker_ring.exs
+++ nodes/ticker_ring.exs
@@ -1,7 +1,29 @@
 defmodule Client do
+  @next nil
+  @last nil
+  @first nil
   def start do
-    pid = spawn(__MODULE__, :ticker, [])
+    pid = spawn(__MODULE__, :get_up, [@first])
+    if @first == nil
+      @first = pid
+    
+    @last = pid
     register pid
   end
-
+  
+  def get_up(next) do
+    receive do
+      {:register, pid} ->
+        get_up(pid)
+      {:tick, pid} ->
+        receive do
+        after 2000 ->
+          send next, {:tick, self}
+        end
+    end
+  end
+  def register(client_pid) do
+    IO.puts "Registering #{client_pid}"
+    send @last, { :register, client_pid}
+  end
 end
