2f22a6501ba3835c8b56a7c9ff63e1ff4a2a7151
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-16 23:59:42 -0600

    Orchestrator for ring

--- nodes/ticker_ring.exs
+++ nodes/ticker_ring.exs
@@ -1,12 +1,19 @@
+defmodule Orchestrator do
+  @name :orchestrator
+  def generator()
+  def start do
+    pid = spawn(__MODULE__, :listener, [[]])
+  end
+  def listener do
+    nil
+  end
+end
 defmodule Client do
-  @next nil
-  @last nil
-  @first nil
   def start do
     pid = spawn(__MODULE__, :get_up, [@first])
-    if @first == nil
+    if @first == nil do
       @first = pid
-    
+    end
     @last = pid
     register pid
   end
@@ -16,6 +23,7 @@ defmodule Client do
       {:register, pid} ->
         get_up(pid)
       {:tick, pid} ->
+        IO.puts "tick received from: #{pid}"
         receive do
         after 2000 ->
           send next, {:tick, self}
