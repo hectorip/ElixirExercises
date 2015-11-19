85ecbb1336a2e6200e63e3b2e309329904acb315
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-19 00:15:14 -0600

    First decenta attempt on ring

--- nodes/ticker_ring.exs
+++ nodes/ticker_ring.exs
@@ -3,19 +3,28 @@ defmodule Orchestrator do
   def generator()
   def start do
     pid = spawn(__MODULE__, :listener, [[]])
+    :global.register_name(@name, pid)
   end
-  def listener do
-    nil
+  def register(pid) do
+    IO.puts "Requested Register for #{pid}"
+    send :global.whereis_name(@name), { :register, pid }
+  end
+  def listener(ring) do
+    receive do
+      { :register, pid } ->
+        if List.first do
+          send List.first  ring, {:register, pid}
+          send pid pid, {:register, List.last}
+        end
+        IO.puts "Registering in ring #{pid}"
+        listner( [ pid | ring ] )
+    end
   end
 end
 defmodule Client do
   def start do
-    pid = spawn(__MODULE__, :get_up, [@first])
-    if @first == nil do
-      @first = pid
-    end
-    @last = pid
-    register pid
+    pid = spawn(__MODULE__, :get_up, [])
+    Orchestrator.resgister(pid)
   end
   
   def get_up(next) do
