8532413195bea0bf256e5979306784fe9ca08abf
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-19 00:46:19 -0600

    Ticker ring seems to be working

--- nodes/ticker_ring.exs
+++ nodes/ticker_ring.exs
@@ -6,36 +6,40 @@ defmodule Orchestrator do
     :global.register_name(@name, pid)
   end
   def register(pid) do
-    IO.puts "Requested Register for #{pid}"
+    IO.puts "Requested Register"
     send :global.whereis_name(@name), { :register, pid }
   end
   def listener(ring) do
     receive do
       { :register, pid } ->
-        if List.first do
-          send List.first  ring, {:register, pid}
-          send pid pid, {:register, List.last}
+        if List.first ring do
+          send (List.first ring), {:register, pid, (length ring) }
+          send pid, {:register, (List.last ring),  (length ring) + 1 }
+        else
+          send pid, {:register, pid, 0}
+          send pid, {:tick, 0}
         end
-        IO.puts "Registering in ring #{pid}"
-        listner( [ pid | ring ] )
+        IO.puts "Registering in ring"
+        listener( [ pid | ring ] )
     end
   end
 end
 defmodule Client do
   def start do
-    pid = spawn(__MODULE__, :get_up, [])
-    Orchestrator.resgister(pid)
+    pid = spawn(__MODULE__, :get_up, [nil, 0])
+    Orchestrator.register(pid)
   end
   
-  def get_up(next) do
+  def get_up(next, n) do
     receive do
-      {:register, pid} ->
-        get_up(pid)
+      {:register, pid, nr} ->
+        get_up(pid, nr)
       {:tick, pid} ->
-        IO.puts "tick received from: #{pid}"
+        IO.puts "tick received from #{pid}"
         receive do
         after 2000 ->
-          send next, {:tick, self}
+          send next, {:tick, n}
+          get_up(next,n)
         end
     end
   end
