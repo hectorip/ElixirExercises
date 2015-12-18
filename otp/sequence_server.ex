9764f22dd902f081e7b0f3b57c6a831fcb40d594
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-17 23:34:34 -0600

    handle_cast

--- otp/sequence_server.ex
+++ otp/sequence_server.ex
@@ -6,6 +6,10 @@ defmodule SequenceServer do
   end
 
   def handle_cast({:increment_number, delta}, current_number) do
-    {:noreply, current_number + delta}
+    { :noreply, current_number + delta }
+  end
+
+  def handle_cast({:decrement_njumber, delta}, current_number) do
+    { :noreply, current_number - 1 } # returns only :noreply and the state of the server
   end
 end
