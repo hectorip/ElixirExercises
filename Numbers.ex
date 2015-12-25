d27da6e646af75a1ba362c8da02a7c99aa42b0ab
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-24 23:59:39 -0600

    Number Docs

--- Numbers.ex
+++ Numbers.ex
@@ -1,5 +1,5 @@
 IO.puts 4/2
-IO.puts 3/2
-IO.puts 1_000_000
-IO.puts div(5,2)
-IO.puts rem(3,2)
+IO.puts 3/2 #normal division
+IO.puts 1_000_000 # underscores can be used to make it more human-readable
+IO.puts div(5,2) # Integer division
+IO.puts rem(3,2) # Modulo
--- PrivateFunc.ex
+++ PrivateFunc.ex
@@ -1,4 +1,6 @@
 defmodule AModule do
+
+  # Private functions are only visible inside the same module
   defp ThisIsAPrivateFucntion(a,b) do
     IO.puts a + b
   end
