38c6e09866bb295acddc661fb6ad8862786bf8f4
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-24 23:35:44 -0600

    Put function in Stack General Server

--- otp/stack_server.ex
+++ otp/stack_server.ex
@@ -7,4 +7,7 @@ defmodule StackServer do
   def handle_call(:pop, _client, [next|rest]) do
     {:reply, next, rest}
   end
+  def handle_cast({:push, element_to_push}, stack) do
+    {:noreply, [ element_to_push | stack ]}
+  end
 end
