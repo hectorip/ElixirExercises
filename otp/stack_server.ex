3e1cd50417e98b373c5ab5aaa2be9c0a375ac523
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-11 23:53:15 -0600

    Cause error function

--- otp/stack_server.ex
+++ otp/stack_server.ex
@@ -9,21 +9,26 @@ defmodule StackServer do
   def push(element) do
     GenServer.cast(:stack_server, {:push, element})
   end
+  def cause_error() do
+    GenServer.call(:stack_server, :push)
+  end
   def handle_call(:pop, _client, []) do
     {:reply, nil , []}
   end
   def handle_call(:pop, _client, [next|rest]) do
     {:reply, next, rest}
   end
-  def handle_cast({:push, 10}, _stack) do
-    raise "Received 10"
+  def handle_call(:push, _client , stack) do
+    {:stop, "WE received push as call", stack}
+    # raise "Received 10"
   end
   def handle_cast({:push, element_to_push}, stack) do
     {:noreply, [ element_to_push | stack ]}
   end
   def terminate(reason, state) do
-    IO.puts "terminating"
+    IO.puts "Terminating Message"
     IO.inspect reason
     IO.inspect state
+    :ok
   end
 end
