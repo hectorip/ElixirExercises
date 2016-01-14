ef16c2c0e160355412e8560105c54f83756b0f91
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-13 23:57:27 -0600

    Supervisor for stack_stash app

--- otp/stack_stash/lib/stack/supervisor.ex
+++ otp/stack_stash/lib/stack/supervisor.ex
@@ -3,5 +3,14 @@ def Stack.Supervisor do
 
   def start_link(initial_stack) do
     result = {:ok, sup} = Supervisor.start_link(__MODULE__, [initial_stack])
+    start_workers(initial_stack, sup)
+    result
+  end
+  def start_workers(initial_stack, supervisor) do
+    {:ok, stash} = Supervisor.start_child(supervisor, worker(Stack.Stash, [initial_stack]))
+    Supervisor.start_child(supervisor, worker(Stack.Server, [stash]))
+  end
+  def init(_) do
+    supervise [], strategy: :one_for_one
   end
 end
