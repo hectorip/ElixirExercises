3dde6901d0e13a367fa0f70fef9d265f8522f1b3
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-14 23:45:45 -0600

    Arity new example

--- Arity.ex
+++ Arity.ex
@@ -3,7 +3,11 @@ defmodule Geometry do
   # A function signature is composed by his name and the
   # number of arguments it receives.
 
-  def fun(a, b \\ 1, c, d \\ 2) do
+  def fun(a, b \\ 1, c \\ 2, d \\ 2) do
     IO.puts a + b + c + d
+  end
+
+  def fun() do
+    IO.puts "Soy rapero"
   end
 end
--- otp/stack_server.ex
+++ otp/stack_server.ex
@@ -1,20 +1,26 @@
 defmodule StackServer do
   use GenServer
+
   def start_link(state) do
     GenServer.start_link(__MODULE__, state, name: :stack_server)
   end
+
   def pop() do
     GenServer.call(:stack_server, :pop)
   end
+
   def push(element) do
     GenServer.cast(:stack_server, {:push, element})
   end
+
   def cause_error() do
     GenServer.call(:stack_server, :push)
   end
-  def handle_call(:pop, _client, []) do
+
+  def handle_call(:pop, _client, [  ]) do
     {:reply, nil , []}
   end
+
   def handle_call(:pop, _client, [next|rest]) do
     {:reply, next, rest}
   end
@@ -22,12 +28,15 @@ defmodule StackServer do
     {:stop, "WE received push as call", stack}
     # raise "Received 10"
   end
-  def handle_cast({:push, n}, stack) when n > 10 do
+
+  def handle_cast({:push, n}, stack) when n < 10 do
     System.halt n
   end
+
   def handle_cast({:push, element_to_push}, stack) do
     {:noreply, [ element_to_push | stack ]}
   end
+
   def terminate(reason, state) do
     IO.puts "Terminating Message"
     IO.inspect reason
