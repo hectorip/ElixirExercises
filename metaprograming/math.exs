285dec1bfaeac5c38a0e8c01c6743a5a2f1c067b
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-19 23:59:15 -0600

    Plus macro

--- metaprograming/math.exs
+++ metaprograming/math.exs
@@ -7,4 +7,13 @@ defmodule Math do
       IO.puts "#{lhs} + #{rhs} is #{result}"
     end
   end
+  defmacro say({:*, _, [lhs, rhs]}) do
+    quote do
+      lhs = unquote(lhs)
+      rhs = unquote(rhs)
+      result = lhs * rhs
+      IO.puts "#{lhs} times #{rhs} is #{result}"
+    end
+  end
+  
 end
