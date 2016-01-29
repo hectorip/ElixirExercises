bcfccbad2b6787ffd9cd70e01b7fc86205704326
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-29 00:04:03 -0600

    random number generation in elixir

---
+++ random.exs
@@ -0,0 +1,15 @@
+# how to get random numbers in Elixir
+
+# first we need to seed the pseudo-random number generator.
+# We are using the current timestamp as a unique number because it
+# can be considered as unique number in an application that has only one
+# process, if we have more processes, in order to avoid number
+# repeitition, we need to look for another unique number to seed.
+
+:random.seed(:os.timestamp)
+
+
+# now, we can ask for a random number, this will return a random number between
+# 0 and 1
+
+:random.uniform
