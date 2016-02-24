c691bcae65a05e1d7f6d81bf27480fe7304ba569
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-24 15:13:33 -0600

    Agents require a function both for start and for retrieve data

--- agents/agents_example.exs
+++ agents/agents_example.exs
@@ -1,7 +1,7 @@
 
-agent = Agent.start_link(fn -> 0 end)
+{:ok, agent} = Agent.start(fn -> 0 end)
 
-val = Agent.get(agent)
+val = Agent.get(agent, &(&1))
 
 IO.puts "Retrieving value from agent"
 IO.puts val
