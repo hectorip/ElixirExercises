bff64e842a1959f8c2081643d794876293297675
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-24 15:34:30 -0600

    Example of a Agent that manages a Map

---
+++ agents/frequency.exs
@@ -0,0 +1,20 @@
+defmodule Frequency do 
+  def start_link do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def add_word(word) do
+    Agent.update(__MODULE__, 
+    fn state ->
+      Map.update(state, word, 1, &(&1 + 1))
+    end)
+  end
+
+  def count_for(word) do 
+    Agent.get(__MODULE__, fn state -> Map.get(state, word, 0) end )
+  end
+
+  def words do
+    Agent.get(__MODULE__, fn state -> Map.keys(state) end)
+  end
+end
