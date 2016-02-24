65bc7a3cc06dcc9041939660fa4156b29cd1220b
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-24 14:48:20 -0600

    Async

--- agents/tasks.exs
+++ agents/tasks.exs
@@ -1,11 +1,11 @@
 defmodule Fib do
-  of(0), do: 0
-  of(1), do: 1
-  of(n), do: of(n-1) + of(n-2)
+  def of(0), do: 0
+  def of(1), do: 1
+  def of(n), do: of(n-1) + of(n-2)
 end
 
 IO.puts "Calling Task"
-task = Task.start_link(Fib.of(37))
+task = Task.async(fn -> Fib.of(37) end)
 
 
 IO.puts "Doing other things"
