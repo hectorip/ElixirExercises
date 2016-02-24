b50d74f2bf14b40e76ceb394eb98fcf0d04b03d8
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-24 15:15:52 -0600

    Updating Agent value

--- agents/agents_example.exs
+++ agents/agents_example.exs
@@ -5,3 +5,8 @@ val = Agent.get(agent, &(&1))
 
 IO.puts "Retrieving value from agent"
 IO.puts val
+
+IO.puts "Retrieving and updating"
+Agent.updata(agent, &(&1*10))
+IO.puts Agent.get(agent, &(&1))
+
