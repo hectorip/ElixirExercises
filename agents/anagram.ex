44e0294fec8ba9b8aa71aa7a26e1aa3e6e024b77
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-26 23:23:40 -0600

    File loader in anagram

--- agents/anagram.ex
+++ agents/anagram.ex
@@ -23,3 +23,18 @@ defmodule Dictionary do
   do: word |> to_char_list |> Enum.sort |> to_string
 
 end
+
+defmodule WordlistLoader do
+
+  def load_from_files(file_names) do
+    file_names
+    |> Stream.map(fn name -> Task.async(fn -> load_task(name) end) end)
+    |> Enum.map(&Task.await/1)
+  end
+
+  def load_task(file_name) do
+    File.Stream!(file_name, [], :line)
+    |> Enum.map(&String.strip/1)
+    |> Dictionary.add_words
+  end
+end
