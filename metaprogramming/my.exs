8e4ad86e4993770a4ea4ff12987f7d2587195f9d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-07 23:59:05 -0600

    Myif completed

--- metaprogramming/my.exs
+++ metaprogramming/my.exs
@@ -1,5 +1,13 @@
 defmodule My do
   def myif(condition, clauses) do
-    
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+
+    case condition do
+      val when val in [false, nil]
+        -> else_clause
+      _otherwhise
+        -> do_clause
+    end
   end
 end
