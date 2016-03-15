31361fdd8cd33ea98527eefc7b4e7555fc9f0eaf
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-14 23:57:41 -0600

    if macro

--- metaprogramming/mymacros.exs
+++ metaprogramming/mymacros.exs
@@ -1,5 +1,14 @@
 defmodule Macros do
-
+  defmacro if(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+    quote do
+      case unquote(condition) do
+        val when val in [false, nil] -> unquote(else_clause)
+        _ -> unquote(do_caluse)
+      end
+    end
+  end
   defmacro unless(condition, clauses) do
     do_caluse = Keyword.get(clauses, :do, nil)
     else_clause = Keyword.get(clauses, :else, nil)
