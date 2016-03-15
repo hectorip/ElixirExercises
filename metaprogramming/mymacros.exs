0711c3f5e9035dd260e8cca2473dbdf1a629aee2
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-15 00:06:28 -0600

    Fixing some errors

--- metaprogramming/mymacros.exs
+++ metaprogramming/mymacros.exs
@@ -7,12 +7,12 @@ defmodule Macros do
     quote do
       case unquote(condition) do
         val when val in [false, nil] -> unquote(else_clause)
-        _ -> unquote(do_caluse)
+        _ -> unquote(do_clause)
       end
     end
   end
   defmacro unless(condition, clauses) do
-    do_caluse = Keyword.get(clauses, :do, nil)
+    do_clause = Keyword.get(clauses, :do, nil)
     else_clause = Keyword.get(clauses, :else, nil)
     quote do
       case unquote(condition) do
@@ -26,7 +26,7 @@ end
 
 defmodule Test do
   # Macros should be imported before using it always
-  use Macros
+  require Macros
 
   def test_if do
     Macros.if 1==2 do
@@ -37,7 +37,7 @@ defmodule Test do
   end
 
   def test_unless do
-    Macros.unless False do
+    Macros.unless false do
       IO.puts "Unless is working!"
     else
       IO.puts "If this is printed, unless does not work"
