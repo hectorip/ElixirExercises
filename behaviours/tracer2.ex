871ad1b0047efed647a8ba7588793061175659ab
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-21 23:58:06 -0600

    Working on Tracer 2

---
+++ behaviours/tracer2.ex
@@ -0,0 +1,12 @@
+defmodule Tracer do
+  
+end
+
+defmodule Test do
+  import Kernel, except: [def: 2]
+  import Tracer, only:   [def: 2]
+
+  def puts_sum_three(a, b, c), do: IO.inspect(a+b+c)
+  def add_list(list),          do: Enum.reduce(list, 0, &(&1+&2))
+
+end
