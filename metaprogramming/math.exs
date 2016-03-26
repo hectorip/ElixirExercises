7ee347248b1d65e2685dcf5f5b90781581123385
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-25 23:59:47 -0600

    Changing wording

--- metaprogramming/math.exs
+++ metaprogramming/math.exs
@@ -1,11 +1,11 @@
 defmodule Math do
-  # Modules that have Macros need to use them
+  # Modules that have Macros need to to be imported with the kerword 'use'
   defmacro say({:+, _, [lhs, rhs]}) do
     quote do
       lhs = unquote(lhs)
       rhs = unquote(rhs)
       result = lhs + rhs
-      IO.puts "#{lhs} + #{rhs} is #{result}"
+      IO.puts "#{lhs} plus #{rhs} is #{result}"
     end
   end
 
