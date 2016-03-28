747dcd8bee5f4205f258351474696ff6e01cc0c9
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-27 23:58:07 -0600

    working with while macro

---
+++ metaprogramming/while.ex
@@ -0,0 +1,15 @@
+defmodule Controls do
+
+
+  defmacro while(condition, code) do
+    quote do
+      for _ in Stream.cycle([:ok]) do
+        if unquote(condition) do
+          unquote(code)
+        end 
+      end
+    end
+  end
+
+
+end
