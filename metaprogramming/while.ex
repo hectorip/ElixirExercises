9cfa8ea50acb0d038d51ccb077b19144fb502e08
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-29 23:58:09 -0600

    I've found the explanation of why the while will not allow to modify external variables.

--- metaprogramming/while.ex
+++ metaprogramming/while.ex
@@ -1,9 +1,12 @@
 defmodule Controls do
 
 
-  defmacro while(condition, code) do
+  defmacro while(condition, do: code) do
+    IO.inspect code
     quote do
       try do
+        # This list comprehension will not allow modify external
+        # variables, we should find another solution.
         for _ <- Stream.cycle([:ok]) do
           if unquote(condition) do
             unquote(code)
