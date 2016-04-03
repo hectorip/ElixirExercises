de3d3f095307c57f9b037367287fff65c6404da8
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-04-02 23:56:41 -0600

    some notes more

--- metaprogramming/while.ex
+++ metaprogramming/while.ex
@@ -7,6 +7,9 @@ defmodule Controls do
       try do
         # This list comprehension will not allow modify external
         # variables, we should find another solution.
+        # so, we cannot use something like i > 10 as the
+        # ermination condition modifying i inside. But anyways,
+        # that is no functional style
         for _ <- Stream.cycle([:ok]) do
           if unquote(condition) do
             unquote(code)
